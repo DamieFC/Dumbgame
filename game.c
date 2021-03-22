f854b8613270eb8db6e6bb0e2957482b53046b4c
GNUOverYooouu <69485115+DamieFC@users.noreply.github.com>
2021-03-22 16:10:19 -0700

    Create game.c

---
+++ game.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void){
+    printf("~~The universe has been destroyed and there isn't anything left.~~\n");
+    printf("Error code: 404 Universe Not Found.\n");
+    return 0;
+}
